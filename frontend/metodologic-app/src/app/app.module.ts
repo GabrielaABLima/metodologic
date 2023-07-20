83b902820593e9312ccf56aac9b85bf4957c4a13
GabrielaABLima <gabriela.andrade.bueno.lima@gmail.com>
2023-07-20 11:26:21 -0300

    Creating menu

--- frontend/metodologic-app/src/app/app.module.ts
+++ frontend/metodologic-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
