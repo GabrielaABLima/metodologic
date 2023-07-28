df204cc88e8dfe37ca150650a2185e2a5aeca863
GabrielaABLima <gabriela.andrade.bueno.lima@gmail.com>
2023-07-27 23:52:06 -0300

    Added register page

--- frontend/metodologic-app/src/app/home/register/register.module.ts
+++ frontend/metodologic-app/src/app/home/register/register.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; // Importe o MatInputModule do Angular Material
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register/register.component';
@@ -11,7 +15,11 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatIconModule
   ]
 })
 export class RegisterModule { }
